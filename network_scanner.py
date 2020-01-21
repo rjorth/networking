c1b5f85aa89ae61bb24c18073a52d8f24c1c27d5
Rose Orth <rosaliejorth@gmail.com>
2020-01-21 20:44:47 +0100

    receiving packets

--- network_scanner.py
+++ network_scanner.py
@@ -5,13 +5,10 @@ import scapy.all as scapy
 def scan(ip):
 	#discover clients on the same network 
 	arp_req = scapy.ARP(pdst=ip)
-	arp_req.show()
 	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-	broadcast.show()
-	#scapy.ls(scapy.Ether())
 	arp_req_broadcast = broadcast/arp_req
-	print(arp_req_broadcast.summary())
-	arp_req_broadcast.show()
+	answered, unanswered = scapy.srp(arp_req_broadcast, timeout=1)
+	print(answered.summary())
 
 # >> route -n to check ip of router
 # /24 to check the whole subnet (0 to 254)
