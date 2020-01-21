541bfb563c740471a210b2232ad36bd3ff97c544
Rose Orth <rosaliejorth@gmail.com>
2020-01-21 20:51:24 +0100

    iteration

--- network_scanner.py
+++ network_scanner.py
@@ -7,9 +7,12 @@ def scan(ip):
 	arp_req = scapy.ARP(pdst=ip)
 	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 	arp_req_broadcast = broadcast/arp_req
-	answered, unanswered = scapy.srp(arp_req_broadcast, timeout=1)
-	print(answered.summary())
+	answered = scapy.srp(arp_req_broadcast, timeout=1)[0] #only want answered
 
+	for element in answered:
+		print(element[1].psrc)
+		print(element[1].hwsrc)
+		print('-------------------------------------------')
 # >> route -n to check ip of router
 # /24 to check the whole subnet (0 to 254)
 scan("10.0.2./24")
