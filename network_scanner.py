87a5dfaa98de774ebde15d97a5d2783a47df1370
Rose Orth <rosaliejorth@gmail.com>
2020-01-21 06:37:38 +0100

    clean

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,8 @@ import scapy.all as scapy
 
 def scan(ip):
 	#discover clients on the same network 
-	scapy.arping(ip)
+	arp_req = scapy.ARP(pdst=ip)
+	print(arp_req())
 
 # >> route -n to check ip of router
 # /24 to check the whole subnet (0 to 254)
